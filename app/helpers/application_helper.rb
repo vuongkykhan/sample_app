bd0337683c50b281f36b7789cb5ee7f8cdfab00e
vuongkykhan <vuongkykhan@gmail.com>
2018-04-24 16:35:07 +0700

    Chapter_4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Comment
 module ApplicationHelper
+# Return the fulltitle on a per-paget title
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+   end
 end
