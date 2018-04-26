80b3bff3e62401767abc559062bf317f483cad43
vuongkykhan <vuongkykhan@gmail.com>
2018-04-26 14:04:40 +0700

    Chapter 7

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: {
+        user: {
+          name: "  ",
+          email: "user@invalid",
+          password: "123456",
+          password_confirmation: "1234567"
+        }
+      }
+    end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.nil?
+  end
+end
