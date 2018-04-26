de8ae45a33930b4bee7326b4d9b8e1d35a26a779
vuongkykhan <vuongkykhan@gmail.com>
2018-04-26 14:04:40 +0700

    Chapter 8 Fix views with I18n

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,6 @@
 # Comment
 module UsersHelper
   def gravatar_for user, size: 80
-    # Returns the  Gravatar for the given user
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: Settings.user.name.max_length}
   before_save{email.downcase!}
   has_secure_password
+
+  def self.digest string
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
