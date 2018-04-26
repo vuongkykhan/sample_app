a15da328f0f937563987af47db9626e77da3ec85
vuongkykhan <vuongkykhan@gmail.com>
2018-04-26 11:15:30 +0700

    Chapter 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # User Class
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   validates :email, presence: true, length: {maximum: Settings.user.email.max_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: Settings.user.password.min_length}, allow_nil: true
@@ -27,7 +27,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update(remember_digest: User.digest(remember_token))
+    update remember_digest: User.digest(remember_token)
   end
 
   def authenticated? attribute, token
@@ -37,7 +37,7 @@ class User < ApplicationRecord
   end
 
   def forget
-    update(remember_digest: nil)
+    update remember_digest: nil
   end
 
   def activate
@@ -48,6 +48,19 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.user.time.expired.hours.ago
+  end
+
   private
 
   def downcase_email
@@ -56,6 +69,6 @@ class User < ApplicationRecord
 
   def create_activation_digest
     self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
+    self.activation_digest = User.digest activation_token
   end
 end
